ba774c44360d04bce8a279451e145f64a89d6aee
Michał <mmichalf@gmail.com>
2013-04-03 14:32:39 +0200

    change idea

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -2,195 +2,63 @@
  *
  * @author Michal
  */
-
-class Tree
+public class Knapsack 
 {
-    private TreeNode treeRoot;
-//    private int count;
     
-    Tree()
-    {
-        treeRoot = null;
-    }
-    
-    int size()
-    {
-        return treeRoot.size();
-    }
-    
-    int height()
-    {
-        return treeRoot.getHeight();
-    }
-    
-    void   print()
-    {
-        treeRoot.print();
-    }
-    
-    Boolean empty()
-    {
-        return treeRoot.empty();
-    }
-    
-//    //Object root() ;
-//    TreeNode root()
-//    {
-//        
-//    }
-    
-    TreeNode findNode(int w,int h)
-    {
-        return treeRoot.find(w, h);
-    }
-   
-    Boolean insert(int width, int height)
-    {
-        return this.insert(width, height,0,0);
-    }
-        
-    Boolean insert(int width, int height, int xCoordinate, int yCoordinate)
-    {
-        if ( /*empty()*/ treeRoot == null ) 
-        {
-            treeRoot = new TreeNode( width, height, xCoordinate, yCoordinate);
-            return true;
-        }
-        else
-        {
-            TreeNode node = treeRoot.find(width, height);
-            if (node==null)
-            {
-                return false;
-            }
-            else
-            {
-                node.split(width,height);
-                return true;
-            }
-        }
-    }
 }
 
-class TreeNode
+
+class Rectangle
 {
-    private int width;
-    private int height;
     private int x_pos;
     private int y_pos;
-    private Boolean filled;
-    private TreeNode leftNode;
-    private TreeNode rightNode;
+    private int width;
+    private int height;
+    private int area;
     
-    TreeNode( int w, int h, int xCoordinate, int yCoordinate )
+    Rectangle(int w, int h, int x, int y)
     {
         width = w;
         height = h;
-        x_pos = xCoordinate;
-        y_pos = yCoordinate;
-        filled = Boolean.FALSE;
-        leftNode = null;
-        rightNode = null;
+        
+        SetX(x);
+        SetY(y);
+        
+        area = width * height;
     }
     
-    int retrieveWidth()
+    public void SetX(int x)
     {
-        return width;
+        x_pos = x;
     }
     
-    int retrieveHeight()
+    public void SetY(int y)
     {
-        return height;
+        y_pos = y;
     }
     
-    int retrievePositionX()
+    public int GetX()
     {
         return x_pos;
     }
     
-    int retrievePositionY()
+    public int GetY()
     {
         return y_pos;
     }
     
-    TreeNode left()
+    public int GetWidth()
     {
-        return leftNode;
+        return width;
     }
     
-    TreeNode right()
+    public int GetHeight()
     {
-        return rightNode;
+        return height;
     }
     
-    Boolean isFilled()
+    public int Area()
     {
-        return filled;
+        return area;
     }
-    
-    Boolean empty()
-    {
-        return (this == null);
-    }
-    
-    Boolean is_leaf()
-    {
-        return (!empty() && left().empty() && right().empty());
-    }
-    
-    int size()
-    {
-        return (empty() ? 0 :  1 + left().size() + right().size());
-    }
-    
-    int getHeight()
-    {
-        return (empty() ? -1 :1 + Math.max(left().getHeight(), right().getHeight() ));
-    }
-    
-    void print()
-    {
-        System.out.println(retrieveWidth()+" x "+retrieveHeight());
-    
-        if (right()!=null)
-        {
-            System.out.println("Right");
-            right().print();
-        }
-        if (left()!=null) 
-        {
-            System.out.println("Down");
-            left().print();
-        }
-    }
-    
-    void split(int w, int h)
-    {
-        System.out.println(w + ": (" + retrievePositionX() + ", " + retrievePositionY() + ")");
-        filled=Boolean.FALSE;
-        rightNode   = new TreeNode(width-w,h,x_pos+w,y_pos);
-        leftNode    = new TreeNode(width,height-h,x_pos,y_pos+h);
-        //print();
-    }
-    
-    TreeNode find(int w,int h)
-    {
-        if (isFilled())
-        {
-            return ((right().find(w,h)==null)? left().find(w,h): right().find(w,h));
-        }
-        else if (w<=retrieveWidth() &&  h<=retrieveHeight())
-        {
-            return this;
-        }
-        else
-        {
-            return null;
-        }
-    }
-}
-
-public class Knapsack 
-{
-    
 }
